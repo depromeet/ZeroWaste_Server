8533462679f5a847f919396fb36e58ea7e0f7ca5
victoria.k <victoria.k@kakaocorp.com>
2021-04-18 01:04:53 +0900

    user 모델 업데이트

--- zerowaste/apps/user/models/user.py
+++ zerowaste/apps/user/models/user.py
@@ -3,14 +3,15 @@ from django.db import models
 
 
 class User(SoftDeleteModelBase):
-    class Level:
-        INIT = 0
-        levels = (
-            (INIT, 'init'),
-        )
+    # level은 그냥 1부터 계속 쌓으면 될거같아
+    # class Level:
+    #     INIT = 0
+    #     levels = (
+    #         (INIT, 'init'),
+    #     )
 
     name = models.CharField(max_length=8)
     nickname = models.CharField(max_length=10)
-    level = models.SmallIntegerField('state', choices=Level.levels, default=Level.INIT)
+    level = models.SmallIntegerField('level', default=1)
     reported_counts = models.SmallIntegerField(default=0)
     is_notify = models.BooleanField(default=True)
