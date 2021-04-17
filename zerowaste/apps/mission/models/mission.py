b4069ae4fa0863392a0ffa9b68163da938c9435f
victoria.k <victoria.k@kakaocorp.com>
2021-04-18 01:59:57 +0900

    MissionLiker 모델명 변경

--- zerowaste/apps/mission/models/mission.py
+++ zerowaste/apps/mission/models/mission.py
@@ -29,7 +29,9 @@ class Mission(SoftDeleteModelBase):
             (EXTRA_HARD, 'extra_hard'),
         )
 
-    creater_id = models.ForeignKey("user.User", related_name="mission_creater", on_delete=models.DO_NOTHING, db_column="creater_id") # CASCADE 되면 안됨
+    name = models.CharField(max_length=50, default="")
+    creater_id = models.ForeignKey("user.User", related_name="mission_creater", on_delete=models.DO_NOTHING,
+                                   db_column="creater_id")  # CASCADE 되면 안됨
     category = models.SmallIntegerField('category', choices=Category.category, default=Category.RECYCLE)
     # TODO: reported counts -> likes 처럼 관리하기
     reported_counts = models.SmallIntegerField(default=0)
@@ -39,7 +41,9 @@ class Mission(SoftDeleteModelBase):
     content = models.CharField(max_length=1000)
 
 
-class Mission_liker(models.Model):
+class MissionLiker(models.Model):
     id = models.BigAutoField(primary_key=True)
-    mission_id = models.ForeignKey("Mission", related_name="mission_liker_mission", on_delete=models.DO_NOTHING, db_column="mission_id")
-    user_id = models.ForeignKey("user.User", related_name="mission_liker_user", on_delete=models.CASCADE, db_column="user_id")
+    mission_id = models.ForeignKey("Mission", related_name="mission_liker_mission", on_delete=models.DO_NOTHING,
+                                   db_column="mission_id")
+    user_id = models.ForeignKey("user.User", related_name="mission_liker_user", on_delete=models.CASCADE,
+                                db_column="user_id")
