5bf9e04be4564be94c9e1bfefa3c12459d266400
victoria.k <victoria.k@kakaocorp.com>
2021-04-17 12:09:33 +0900

    테스트용 API 추가

--- zerowaste/apps/user/views.py
+++ zerowaste/apps/user/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET'])
+def test(request):
+    return Response(data={'': ''}, status=status.HTTP_200_OK)
