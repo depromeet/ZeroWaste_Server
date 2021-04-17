8d7e9ef26a9a402d36e60f5d74daa48b4c183e1c
victoria.k <victoria.k@kakaocorp.com>
2021-04-17 12:34:34 +0900

    user 모델 추가

---
+++ zerowaste/apps/user/models/user.py
@@ -0,0 +1,16 @@
+from apps.core.models.soft_delete_model_base import SoftDeleteModelBase
+from django.db import models
+
+
+class User(SoftDeleteModelBase):
+    class Level:
+        INIT = 0
+        levels = (
+            (INIT, 'init'),
+        )
+
+    name = models.CharField(max_length=8)
+    nickname = models.CharField(max_length=10)
+    level = models.SmallIntegerField('state', choices=Level.levels, default=Level.INIT)
+    is_forbidden = models.SmallIntegerField(default=0)
+    is_notify = models.BooleanField(default=True)
