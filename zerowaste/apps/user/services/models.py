058ea1f4fb39e723123192b48f1570f5f422e61b
victoria.k <victoria.k@kakaocorp.com>
2021-04-20 15:20:38 +0900

    모델 서비스 로직 작성

---
+++ zerowaste/apps/user/services/models.py
@@ -0,0 +1,21 @@
+from apps.user.models.user import User
+from apps.user.models.auth import Auth
+from apps.core.utils.tools import id_generator
+
+
+def create_anonymous_user():
+    user = User(name="", nickname=id_generator())
+    user.save()
+    return user
+
+
+def get_auth_by_identifier_with_login_type(identifier, login_type):
+    auth = Auth.objects.get(identifier=str(identifier), login_type=login_type)
+    return auth
+
+
+def create_auth(identifier, email, user, social_token, login_type):
+    auth = Auth(identifier=str(identifier), email=email, user_id=user, social_token=social_token,
+                login_type=login_type)
+    auth.save()
+    return auth
