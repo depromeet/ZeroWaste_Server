3fac9f331e609cf0482207486bbb70405d8c19ec
victoria.k <victoria.k@kakaocorp.com>
2021-04-20 15:20:15 +0900

    테스트용 뷰 제거

--- zerowaste/apps/user/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-
-@api_view(['GET'])
-def test(request):
-    return Response(data={'': ''}, status=status.HTTP_200_OK)
