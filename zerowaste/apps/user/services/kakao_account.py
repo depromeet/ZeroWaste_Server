27d9e45d9a4cb97fc26b69c655322ea0be8142f2
victoria.k <victoria.k@kakaocorp.com>
2021-04-29 17:33:13 +0900

    Update kakao_account.py

--- zerowaste/apps/user/services/kakao_account.py
+++ zerowaste/apps/user/services/kakao_account.py
@@ -1,12 +1,14 @@
-import requests
-
+from apps.core.utils.requests import requests_timeout,requests_retry_session
 from apps.core.exceptions import KakaoAccountServerError
 
 
 def get_user_profile(kakao_access_token):
     url = 'https://kapi.kakao.com/v2/user/me'
     headers = {'Authorization': 'Bearer ' + kakao_access_token}
-    response = requests.get(url, headers=headers)
+    response = requests_timeout(requests_retry_session().get,
+                                url=url,
+                                headers=headers,
+                                timeout=3)
 
     if response.status_code != 200:
         raise KakaoAccountServerError(detail=response.json()['msg'])
