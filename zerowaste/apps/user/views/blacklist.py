3d51cbb7fb885ad359b9ea4204470a752f0f5284
WinterBlue16 <leekh090163@gmail.com>
2021-05-13 16:31:12 +0900

    blacklist api post, put, patch 추가

--- zerowaste/apps/user/views/blacklist.py
+++ zerowaste/apps/user/views/blacklist.py
@@ -1,20 +1,61 @@
-from rest_framework import viewsets, mixins, status
-from rest_framework.decorators import api_view, authentication_classes
-from rest_framework.response import Response
-
+from rest_framework import viewsets, mixins, permissions
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from django.utils.decorators import method_decorator
-from django.contrib.auth import get_user_model
 
 from apps.core import permissions as custom_permission
 from apps.user.serializers.models import BlackListSerializer
-from apps.user.models import blacklist
+from apps.user.models.blacklist import BlackList
+from apps.core import constants
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
+@method_decorator(name='create',
+                  decorator=swagger_auto_schema(
+                      tags=['blacklist'],
+                      operation_description="blacklist 기록 생성",
+                      manual_parameters=[
+                          openapi.Parameter(
+                              'Authorization', openapi.IN_HEADER,
+                              type=openapi.TYPE_STRING,
+                              description=constants.USER_JWT_TOKEN
+                          ),
+                      ],
+                      responses={
+                          200: BlackListSerializer,
+                          401: 'Authentication Failed(40100)',
+                          403: 'Permission denied(403)',
+                          404: 'Not found(404)'
+                      }
+                  )
+                )
+
+@method_decorator(name='partial_update',
+                  decorator=swagger_auto_schema(
+                      tags=['blacklist'],
+                      operation_description="Blacklist 정보 업데이트",
+                      manual_parameters=[
+                          openapi.Parameter(
+                              'Authorization', openapi.IN_HEADER,
+                              type=openapi.TYPE_STRING,
+                              description=constants.USER_JWT_TOKEN
+                          ),
+                      ],
+                      responses={
+                          200: BlackListSerializer,
+                          401: 'Authentication Failed(40100)',
+                          403: 'Permission denied(403)',
+                          404: 'Not found(404)'
+                      }
+                  ))
+
 class BlackListViewSet(viewsets.GenericViewSet,
+                       mixins.CreateModelMixin,
+                       mixins.UpdateModelMixin,
                        mixins.RetrieveModelMixin,
                        mixins.ListModelMixin):
+    authentication_classes = [JSONWebTokenAuthentication]
     permission_classes = [custom_permission.IsOwnerOrReadOnly]
-    queryset = get_user_model().objects
+    permission_classes_by_action = {'create' : [permissions.AllowAny]}
+    queryset = BlackList.objects
     serializer_class = BlackListSerializer
