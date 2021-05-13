50f4099abe24ae77f3d0369a9c2bd0ed45ceaccf
WinterBlue16 <leekh090163@gmail.com>
2021-05-13 18:31:11 +0900

    certification api 작성 중

--- zerowaste/apps/mission/serializers/models.py
+++ zerowaste/apps/mission/serializers/models.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from apps.mission.models.mission import Mission
+from apps.mission.models.certification import Certification
 from apps.core.utils.response import build_response_body
 from apps.user.services.models import get_user_by_id
 from apps.user.serializers.models import UserSerializer
@@ -22,6 +23,26 @@ class MissionSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         value = super(MissionSerializer, self).to_representation(instance)
+        creater = get_user_by_id(value['owner'])
+        value['creater'] = UserSerializer(creater).data['data']
+        return build_response_body(data=value)
+
+class CertificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certification
+        fields = ('id', 'name', 'owner', 'mission_id', "image", "content", 'isPublic')
+
+    def validate(self, attrs):
+        return self.initial_data
+
+    def to_internal_value(self, data):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            user = request.user
+            self.initial_data['owner'] = user
+
+    def to_representation(self, instance):
+        value = super(CertificationSerializer, self).to_representation(instance)
         creater = get_user_by_id(value['owner'])
         value['creater'] = UserSerializer(creater).data['data']
         return build_response_body(data=value)
