280e67806b75661f061d08f66f1b11ccdae9ae02
WinterBlue16 <leekh090163@gmail.com>
2021-05-22 19:28:42 +0900

    key값 변경 : signed url 중복 생성 방지

--- zerowaste/apps/core/utils/s3.py
+++ zerowaste/apps/core/utils/s3.py
@@ -7,8 +7,9 @@ from botocore.client import Config
 def sign_upload(counts, user_id):
     s3 = boto3.client('s3', config=Config(signature_version=settings.AWS_SIGNATURE_VERSION, region_name=settings.AWS_REGION))
     result = []
+
     for i in range(counts):
-        key = 'users/' + str(user_id) + '/' + str(int(datetime.now().timestamp()))
+        key = 'users/' + str(user_id) + '/' + str(i) # int(datetime.now().timestamp())
         signed_url = s3.generate_presigned_url(
             'put_object',
             Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': key},
