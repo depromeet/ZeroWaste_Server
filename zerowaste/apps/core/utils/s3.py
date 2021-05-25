424f9af325051e5abb1b7d60e211dae9ed5ddbf6
victoria.k <victoria.k@kakaocorp.com>
2021-05-25 20:16:04 +0900

    generate_upload_signed_url 업데이트

--- zerowaste/apps/core/utils/s3.py
+++ zerowaste/apps/core/utils/s3.py
@@ -1,15 +1,16 @@
 import boto3
-from datetime import datetime
 from django.conf import settings
 from botocore.client import Config
 
+from apps.core.utils.tools import id_generator
 
-def sign_upload(counts, user_id):
+
+def generate_upload_signed_url(counts, user_id):
     s3 = boto3.client('s3', config=Config(signature_version=settings.AWS_SIGNATURE_VERSION, region_name=settings.AWS_REGION))
     result = []
 
     for i in range(counts):
-        key = 'users/' + str(user_id) + '/' + str(i) # int(datetime.now().timestamp())
+        key = 'users/' + str(user_id) + '/' + id_generator(size=10)
         signed_url = s3.generate_presigned_url(
             'put_object',
             Params={'Bucket': settings.AWS_STORAGE_BUCKET_NAME, 'Key': key},
