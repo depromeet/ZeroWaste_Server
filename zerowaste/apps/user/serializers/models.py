a568e47f9ad663f4d275c936601a02ba0808ed1b
victoria.k <victoria.k@kakaocorp.com>
2021-05-26 14:56:00 +0900

    user like 정보 내려주기

--- zerowaste/apps/user/serializers/models.py
+++ zerowaste/apps/user/serializers/models.py
@@ -5,7 +5,7 @@ from apps.user.models.user import User
 from apps.mission.models.participation import Participation
 from apps.user.models.blocklist import BlockList
 from apps.core.utils.response import build_response_body
-from apps.mission.services.models import get_participations_by_owner
+from apps.mission.services.models import get_participations_by_owner, get_liked_missions_by_owner
 
 
 class AuthSerializer(serializers.ModelSerializer):
@@ -23,6 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
         value = super(UserSerializer, self).to_representation(instance)
         value['completed_mission_counts'] = len(get_participations_by_owner(instance))
         value['progressing_mission_counts'] = len(get_participations_by_owner(instance, status=Participation.Status.READY))
+        value['liked_mission_counts'] = len(get_liked_missions_by_owner(instance))
         return build_response_body(data=value)
 
 
