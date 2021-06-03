122584917bd93dc6d21a6942cd8b2a84c78d4c45
victoria.k <victoria.k@kakaocorp.com>
2021-06-04 00:45:39 +0900

    ready_queryset 처리 추가

--- zerowaste/apps/mission/services/cron_job.py
+++ zerowaste/apps/mission/services/cron_job.py
@@ -18,7 +18,13 @@ def update_participation_status():
 
         Participation.objects.bulk_update(participated_querysets, ['status', 'is_cron_checked'])
 
-        # ready_querysets = get_participations_after_end_date(status=Participation.Status.READY)
+        ready_querysets = get_participations_after_end_date(status=Participation.Status.READY)
+        for ready_queryset in ready_querysets:
+            logger.debug(f'{ready_queryset}')
+            ready_queryset.status = Participation.Status.FAILURE
+            ready_queryset.is_cron_checked = True
+
+        Participation.objects.bulk_update(ready_querysets, ['status', 'is_cron_checked'])
 
         # TODO: all mission을 업데이트하는게 아니라, 위에 업데이트한 미션들만 찾아서 업데이트하는 방법으로 해야함. set 이용 가능한지 테스팅해보기.
         missions = Mission.objects.all()
